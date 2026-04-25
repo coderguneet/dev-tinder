fe40e541f71f3667fdbb54571f5539edf76ee4fe
coderguneet <coderguneet@gmail.com>
2026-04-25 17:59:47 +0530

    removed exposed env vbls

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
 async function connectDB(){
-    await mongoose.connect('mongodb+srv://guneet:1U4EXaaL6tryQtuR@namastenode.0ba0su1.mongodb.net/devTinder');    
+    await mongoose.connect('');    
 }
 module.exports ={connectDB}
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,49 @@
 const mongoose=require('mongoose');
 const userSchema=new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:3,
+        maxLength:50,
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
     },
     password:{
         type:String
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        max:100,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if (!['male','female','others'].includes(value)){
+                throw new Error('gender data not valid');
+                
+            }
+        }
     },
-})
+    photoUrl:{
+        type:String,
+        default:"https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png"
+    },
+    skills:{
+        type:[String],
+        default:"fun"
+    }
+},
+{   
+    timestamps:true,
+}
+);
 const User= mongoose.model("User",userSchema);
 module.exports=User;
